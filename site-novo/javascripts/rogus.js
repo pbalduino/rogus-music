cce25132062b910cd44e4802ad16b24feb6697a3
Plinio Balduino <pbalduino+github@gmail.com>
2012-10-06 02:11:02 -0300

    Separated ways

---
+++ site-novo/javascripts/rogus.js
@@ -0,0 +1,20 @@
+function moneyTextToFloat(text) {
+	var cleanText = text.replace("R$ ", "").replace(",", ".");
+	return parseFloat(cleanText);
+}
+
+function floatToMoneyText(value) {
+	var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
+	text = "R$ " + text;
+	return text.substr(0, text.length - 2) + "," + text.substr(-2);
+}
+
+function readTotal() {
+	var total = document.getElementById("total");
+	return moneyTextToFloat(total.innerHTML);
+}
+
+function writeTotal(value) {
+	var total = document.getElementById("total");
+	total.innerHTML = floatToMoneyText(value);
+}
